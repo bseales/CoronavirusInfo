a18b0c1f73639f0999ca1bab773c1e430beb6c1e
Batson Seales <batson.seales2@gmail.com>
2020-03-31 21:19:58 -0500

    added graph, hardcoded to italy for now

--- coronavirus.js
+++ coronavirus.js
@@ -4,11 +4,7 @@ $(document).ready(function() {
         url: "https://api.covid19api.com/summary",
         type: "GET",
         success: function(result) {
-            //alert();
             console.log(result);
-            // _.each(result.Countries, function(country) {
-            //     $("#countries").append("<div>" + country.Country + "</div>");
-            // })
             var dataSet = [];
             var slugsDone = [];
             _.each(result.Countries, function(country) {
@@ -39,6 +35,85 @@ $(document).ready(function() {
                     { "title": 'Total Recovered', data: 'TotalRecovered', "orderSequence": [ "desc", "asc"]  }
                 ]
             });
+
+            var countryConfirmed = [];
+            var countryRecovered = [];
+            var countryDied = [];
+
+            // Get the US Stats by default.
+            $.ajax({
+                url: "https://api.covid19api.com/total/country/italy/status/deaths",
+                type: "GET",
+                success: function(result) {
+                    countryDied = result;
+                    $.ajax({
+                        url: "https://api.covid19api.com/total/country/italy/status/confirmed",
+                        type: "GET",
+                        success: function(result1) {
+                            countryConfirmed = result1;
+                            $.ajax({
+                                url: "https://api.covid19api.com/total/country/italy/status/recovered",
+                                type: "GET",
+                                success: function(result2) {
+                                    countryRecovered = result2;
+
+                                    var labels = [];
+                                    _.each(countryConfirmed, function(confirmed) {
+                                        labels.push(confirmed.Date.substring(0,10))
+                                    });
+
+                                    var confirmedNums = [];
+                                    _.each(countryConfirmed, function(confirm) {
+                                        confirmedNums.push(confirm.Cases);
+                                    });
+
+                                    var recoveredNums = [];
+                                    _.each(countryRecovered, function(recover) {
+                                        recoveredNums.push(recover.Cases);
+                                    });
+
+                                    var diedNums = [];
+                                    _.each(countryDied, function(died) {
+                                        diedNums.push(died.Cases);
+                                    });
+
+                                    new Chart(document.getElementById("line-chart"), {
+                                        type: 'line',
+                                        data: {
+                                          labels: labels,
+                                          datasets: [
+                                            {
+                                                data: confirmedNums,
+                                                label: "Cases",
+                                                borderColor: "#3e95cd",
+                                                fill: false
+                                            },
+                                            { 
+                                              data: diedNums,
+                                              label: "Died",
+                                              borderColor: "#FF0000",
+                                              fill: false
+                                            }, { 
+                                              data: recoveredNums,
+                                              label: "Recovered",
+                                              borderColor: "#3cba9f",
+                                              fill: false
+                                            }, 
+                                          ]
+                                        },
+                                        options: {
+                                          title: {
+                                            display: true,
+                                            text: 'Italy'
+                                          }
+                                        }
+                                      });
+                                }
+                            })
+                        }
+                    })
+                }
+            })
         }
       });
 });
